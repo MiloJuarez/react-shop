794321f0f560f82fa25d22aea40e3c7545170737
Milo Juarez <ermilo98go@gmail.com>
2022-02-02 15:24:25 -0600

    Se agrego y  configuro CSS y Sass

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles/global.scss";
 
 const App = () => {
     return <h1>Hola Milo, Bienvenido al Curso Practico de React!</h1>;
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: "./src/index.js",
@@ -26,6 +27,10 @@ module.exports = {
                     loader: "html-loader",
                 },
             },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
         ],
     },
     plugins: [
@@ -33,5 +38,15 @@ module.exports = {
             template: "./public/index.html",
             filename: "index.html",
         }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+        }),
     ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist"),
+        },
+        port: 3005,
+        compress: true,
+    },
 };

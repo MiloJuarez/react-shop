bc622793e56800d681cbf80c6b93edfd148c4dcd
Milo Juarez <ermilo98go@gmail.com>
2022-02-10 14:32:52 -0600

    Refactor de estilos de Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import "../styles/Home.scss";
 const Home = () => {
     return (
         <section class="Home-container">
-            <div class="Home-cards-container">
+            <div class="Home-cards">
                 <ProductCard
                     product={{
                         image: "https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",

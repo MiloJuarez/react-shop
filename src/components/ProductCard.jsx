848ba49184ed92dd4c91e315db626d0554c1a7af
Milo Juarez <ermilo98go@gmail.com>
2022-02-10 11:29:06 -0600

    Uso de convencion BEM en ProductCard

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,16 +4,25 @@ import "../styles/ProductCard.scss";
 const ProductCard = ({ product }) => {
     return (
         <article className="ProductCard">
-            <img src={product.image} alt={product.name} />
+            <img
+                src={product.image}
+                alt={product.name}
+                className="ProductCard__image"
+            />
             <div className="ProductCard-info">
                 <div>
-                    <p>${product.price}</p>
-                    <p>{product.name}</p>
+                    <p className="ProductCard-info__text ProductCard-info__text--bold">
+                        ${product.price}
+                    </p>
+                    <p className="ProductCard-info__text ProductCard-info__text--gray">
+                        {product.name}
+                    </p>
                 </div>
-                <figure>
+                <figure className="ProductCard-info__figure">
                     <img
                         src="./assets/icons/bt_add_to_cart.svg"
                         alt="Add to cart"
+                        className="ProductCard-info__figure--icon-add"
                     />
                 </figure>
             </div>

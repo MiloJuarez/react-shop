18ae62f31e868208a5b1054aa06f9d12d58fd01d
Milo Juarez <ermilo98go@gmail.com>
2022-02-10 18:41:03 -0600

    Correcion de clases y uso de alias en Login

--- src/components/SecondaryButton.jsx
+++ src/components/SecondaryButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "@styles/SecondaryButton.scss";
 
 const SecondaryButton = ({ label }) => {
-    return <button class="SecondaryButton">{label}</button>;
+    return <button className="SecondaryButton">{label}</button>;
 };
 
 export default SecondaryButton;
--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,31 +1,28 @@
 import React from "react";
-import "../styles/Login.scss";
-import PrimaryInputSubmit from "../components/PrimaryInputSubmit";
-import SecondaryButton from "../components/SecondaryButton";
+import "@styles/Login.scss";
+import PrimaryInputSubmit from "@components/PrimaryInputSubmit";
+import SecondaryButton from "@components/SecondaryButton";
+import logo from "@logos/logo_yard_sale.svg";
 
 const Login = () => {
     return (
-        <div class="Login">
-            <div class="Login-container">
-                <img
-                    src="./assets/logos/logo_yard_sale.svg"
-                    alt="Logo"
-                    class="Login__icon"
-                />
+        <div className="Login">
+            <div className="Login-container">
+                <img src={logo} alt="Logo" className="Login__icon" />
 
-                <form action="/" class="Login-form">
-                    <label for="email" class="Login-form__label">
+                <form action="/" className="Login-form">
+                    <label htmlFor="email" className="Login-form__label">
                         Email address
                     </label>
                     <input
                         type="email"
                         name="email"
                         id="email"
-                        class="Login-form__input Login-form__input--mb22"
+                        className="Login-form__input Login-form__input--mb22"
                         placeholder="camilayokoo@gmail.com"
                     />
 
-                    <label for="password" class="Login-form__label">
+                    <label htmlFor="password" className="Login-form__label">
                         Password
                     </label>
                     <input
@@ -33,7 +30,7 @@ const Login = () => {
                         name="password"
                         id="password"
                         placeholder="*********"
-                        class="Login-form__input Login-form__input--mb22"
+                        className="Login-form__input Login-form__input--mb22"
                     />
 
                     <PrimaryInputSubmit value={"Log in"} />

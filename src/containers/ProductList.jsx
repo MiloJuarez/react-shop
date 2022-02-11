153bba7626b8d720fddcea4018bdd59b5bbaf798
Milo Juarez <ermilo98go@gmail.com>
2022-02-11 12:39:42 -0600

    Custom hook para obtener los productos del API

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,18 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "@styles/ProductList.scss";
 import ProductCard from "@components/ProductCard";
-import axios from "axios";
+import useGetProducts from "../hooks/useGetProdutcs";
 
 const API = "https://api.escuelajs.co/api/v1/products";
 
 const ProductList = () => {
-    const [products, setProducts] = useState([]);
-
-    useEffect(async () => {
-        const response = await axios(API);
-        setProducts(response.data);
-    }, []);
-
+    const products = useGetProducts(API);
     return (
         <section className="ProductList-container">
             <div className="ProductList-cards">

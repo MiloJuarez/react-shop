b84137064ae4c6c5543d5a3853b4e8440f5d735b
Milo Juarez <ermilo98go@gmail.com>
2022-02-11 11:36:33 -0600

    Creacion del container ProductList

---
+++ src/containers/ProductList.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+import "@styles/ProductList.scss";
+import ProductCard from "@components/ProductCard";
+
+const ProductList = () => {
+    return (
+        <section className="ProductList-container">
+            <div className="ProductList-cards">
+                <ProductCard
+                    product={{
+                        image: "https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+                        name: "Bike",
+                        price: 60.0,
+                    }}
+                />
+
+                <ProductCard
+                    product={{
+                        image: "https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+                        name: "Bike",
+                        price: 60.0,
+                    }}
+                />
+
+                <ProductCard
+                    product={{
+                        image: "https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+                        name: "Bike",
+                        price: 60.0,
+                    }}
+                />
+
+                <ProductCard
+                    product={{
+                        image: "https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+                        name: "Bike",
+                        price: 60.0,
+                    }}
+                />
+
+                <ProductCard
+                    product={{
+                        image: "https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+                        name: "Bike",
+                        price: 60.0,
+                    }}
+                />
+
+                <ProductCard
+                    product={{
+                        image: "https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+                        name: "Bike",
+                        price: 60.0,
+                    }}
+                />
+
+                <ProductCard
+                    product={{
+                        image: "https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+                        name: "Bike",
+                        price: 60.0,
+                    }}
+                />
+
+                <ProductCard
+                    product={{
+                        image: "https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+                        name: "Bike",
+                        price: 60.0,
+                    }}
+                />
+
+                <ProductCard
+                    product={{
+                        image: "https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+                        name: "Bike",
+                        price: 60.0,
+                    }}
+                />
+
+                <ProductCard
+                    product={{
+                        image: "https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+                        name: "Bike",
+                        price: 60.0,
+                    }}
+                />
+
+                <ProductCard
+                    product={{
+                        image: "https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+                        name: "Bike",
+                        price: 60.0,
+                    }}
+                />
+
+                <ProductCard
+                    product={{
+                        image: "https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+                        name: "Bike",
+                        price: 60.0,
+                    }}
+                />
+            </div>
+        </section>
+    );
+};
+
+export default ProductList;

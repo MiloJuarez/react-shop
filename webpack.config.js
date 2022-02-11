92769d0932607129c9cdf22919f697cf332f69fb
Milo Juarez <ermilo98go@gmail.com>
2022-02-10 18:19:11 -0600

    Importacion de imagenes y configuracion de alias en webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,14 @@ module.exports = {
     mode: "development",
     resolve: {
         extensions: [".js", ".jsx"],
+        alias: {
+            "@components": path.resolve(__dirname, "src/components/"),
+            "@containers": path.resolve(__dirname, "src/containers/"),
+            "@pages": path.resolve(__dirname, "src/pages/"),
+            "@styles": path.resolve(__dirname, "src/styles/"),
+            "@icons": path.resolve(__dirname, "src/assets/icons/"),
+            "@logos": path.resolve(__dirname, "src/assets/logos/"),
+        },
     },
     module: {
         rules: [
@@ -31,6 +39,10 @@ module.exports = {
             {
                 test: /\.(css|scss)$/i,
                 use: ["style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                type: "asset",
             },
         ],
     },
